275dc677db3495f1811aad34e53e1b9891283e17
VICTOR GERMAN HICHEZ ROSARIO <2021-0022@uteco.edu.do>
2024-02-01 17:08:32 -0500

    Agregar archivos de proyecto.

---
+++ PerfilActores/Data/Actor.cs
@@ -0,0 +1,10 @@
+﻿namespace PerfilActores.Data
+{
+    public class Actor
+    {
+        public string Nombre { get; set; }
+        public string Biografia { get; set; }
+        public string Foto { get; set; }
+
+    }
+}
